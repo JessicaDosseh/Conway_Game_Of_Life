4185a150d4a1e823da71a60ff61ee5f474585868
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-10-26 18:16:23 -0400

    🖌️   styling - Game layout

--- conway_game_of_life/src/App.js
+++ conway_game_of_life/src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import Grid from './Components/Grid';
 import Banner from './Components/Banner'; 
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
+    <Game>
       <Banner/>
       <br/>
       <br/>
       <Grid/>
-    </div>
+    </Game>
   );
 }
+
+const Game = styled.div`
+  display: flex:
+  flex-flow: column wrap;
+  align-content: center;
+  justify-content: center;
+  justify-item: center;
+  // width: 60%;
+  background: #FAFAFA;
+  padding: 5% 20% 5% 20%;
+`
 
 export default App;
--- conway_game_of_life/src/Components/Grid.js
+++ conway_game_of_life/src/Components/Grid.js
@@ -1,9 +1,11 @@
 import React, {useState, useCallback, useRef} from 'react';
 import produce from 'immer';
 import cellNeighbors from './Neighbors'; 
+import styled from 'styled-components';
 import generateEmptyGrid from '../Functions/generateEmptyGrid'; 
 import generateRandomGrid from '../Functions/generateRandomGrid';
 import Button from './Button';
+import game_of_life_logo from '../Images/game_of_life_logo.png';
 
 // Grid Dimensions 
 const Rows = 25;
@@ -13,6 +15,8 @@ const Cols = 25;
 const CellColor = 'pink'; 
 const SimulationSpeed = 100; 
 const FillRatio = 0.7; 
+const CellShape = 0; 
+const CellSize = '20px'; // square
 
 function Grid() {
   // Grid State | use state to build out the grid 
@@ -80,38 +84,11 @@ function Grid() {
     }, [])
 
   return (
-    <div>
-      {/* Button Components */}
-      <Button 
-        title={running ? 'Stop' : 'Start'}
-        click={() => {
-          setRunning(!running); 
-          if (!running) {
-            runningRef.current = true;
-            runSimulation();
-          } 
-        }}
-      />
-
-      <Button
-        title={'Clear'}
-        click={() => {
-          setGrid(generateEmptyGrid(Rows, Cols)); 
-        }}
-      />
-
-      <Button
-        title={'Random'}
-        click={() => {
-          setGrid(generateRandomGrid(Rows, Cols, FillRatio));
-        }}
-      />
-    
-      {/* Grid Rendering Component */}
-      <div className="App"
+    <Box>
+      <GridBox className="App"
         style={{
           display: 'grid',
-          gridTemplateColumns: `repeat(${Cols}, 20px)`
+          gridTemplateColumns: `repeat(${Cols}, ${CellSize})`
         }}
       >
         {grid.map((rows, rowIndex) => 
@@ -126,17 +103,148 @@ function Grid() {
               setGrid(newGrid); 
             }}
             style={{
-              width: 20, 
-              height: 20,
+              width: CellSize, 
+              height: CellSize,
               background: grid[rowIndex][colIndex] ? CellColor : undefined,
               border: 'solid 1px gray',
-              // borderRadius: '50px', // circles
+              borderRadius: CellShape,
             }} 
           />)
         )}
-      </div>
-    </div>
+      </GridBox>
+
+      <VerticalLine/>
+
+      <Description>
+        <Title>
+          <img src={game_of_life_logo} width='30' height='30' style={{padding: '13px'}}/>
+          <h3>Play Conway's Game of Life</h3>
+        </Title>
+
+        <About>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in </p>
+        </About>
+        
+        <br/>
+
+        <Actions>
+          <ButtonBox>
+            <br/>
+            <Button 
+              title={running ? 'Stop' : 'Start'}
+              click={() => {
+                setRunning(!running); 
+                if (!running) {
+                  runningRef.current = true;
+                  runSimulation();
+                } 
+              }}
+            />
+
+            <br/>
+            <br/>
+
+            <Button
+              title={'Clear'}
+              click={() => {
+                setGrid(generateEmptyGrid(Rows, Cols)); 
+              }}
+            />
+
+            <br/>
+            <br/>
+
+            <Button
+              title={'Random'}
+              click={() => {
+                setGrid(generateRandomGrid(Rows, Cols, FillRatio));
+              }}
+            />
+          </ButtonBox>  
+
+          <LinkBox>
+            <p style={{color: "gray"}}>Feature Settings</p>
+            <hr/>
+            <p>Default</p>
+            <p>Custome</p>
+
+            <br/>
+
+            <p style={{color: "gray"}}>Info</p>
+            <hr/>
+            <p>Play Game</p>
+            <p>Rules</p>
+            <p>About This Project</p>
+          </LinkBox>
+        </Actions>
+
+      </Description>
+    </Box>
   );
 }
+
+
+const Box = styled.div`
+  background: #ffffff;
+  display: flex;
+  flex-flow: row wrap; 
+  justify-content: space-evenly;
+  align-item: center;
+  width: 100%;
+  padding 50px 10px 50px 10px;
+`
+
+const GridBox = styled.div`
+  background: #ffffff;
+  border: 2px solid black;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  padding-right: 2px;
+  margin: 20px;
+`
+
+const VerticalLine = styled.div`
+  border-left: 2px solid #EAEAEA;
+  height: 600px;
+  left: 50%;
+  margin-left: 10px;
+`
+
+const Description = styled.div`
+  width: 50%;
+  display: flex; 
+  flex-flow: column wrap;
+  justify-content: flex-start;
+`
+
+const Title = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  padding-left: 45px;
+`
+
+const About = styled.div`
+  width: 80%; 
+  line-height: 150%;
+  padding-left: 60px;
+`
+
+const Actions = styled.div`
+  display: flex;
+  flex-flow: row wrap; 
+  justify-content: center;
+  align-item: center;
+  width: 100%;
+`
+
+const LinkBox = styled.div`
+  width: 35%; 
+`
+
+const ButtonBox = styled.div`
+  width: 40%;
+  display: flex; 
+  flex-flow: column wrap;
+  margin-right: 50px;
+`
 
 export default Grid;
